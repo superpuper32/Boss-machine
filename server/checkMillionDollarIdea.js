8bb772e95bfde53e4a10720b305ce6f2039fbbaa
superpuper32 <pochitalin_m@mail.ru>
2018-05-16 19:22:45 +0300

    Ideas routes

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,6 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (req, res, next) => {
+  
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
--- server/ideas.js
+++ server/ideas.js
@@ -4,3 +4,51 @@ module.exports = ideasRouter;
 
 const { getAllFromDatabase, getFromDatabaseById, addToDatabase,
   updateInstanceInDatabase, deleteFromDatabasebyId } = require('./db');
+
+ideasRouter.get('/', (req, res, next) => {
+  res.send(getAllFromDatabase('ideas'));
+});
+
+ideasRouter.get('/:ideaId', (req, res, next) => {
+  const idea = getFromDatabaseById('ideas', req.params.ideaId);
+  if (idea) {
+    res.send(idea);
+  } else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.put('/:IdeaId', (req, res, next) => {
+  const idea = getFromDatabaseById('ideas', req.params.ideaId);
+  if (idea) {
+    let updatedIdea = updateInstanceInDatabase('ideas', req.body);
+    res.send(updatedIdea);
+  } else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.post('/:ideaId', (req, res, next) => {
+  const idea = getFromDatabaseById('ideas', req.params.ideaId);
+  if (idea) {
+    const createIdea = addToDatabase('ideas', req.body);
+    res.status(201).send(createIdea);
+  } else {
+    res.status(404).send();
+  }
+});
+
+ideasRouter.delete('/:ideaId', (req, res, next) => {
+  const idea = getFromDatabaseById('ideas', req.params.ideaId);
+  if (idea) {
+    let deleteIdea = deleteFromDatabasebyId('ideas', req.params.ideaId);
+    if (deleteIdea) {
+      res.status(204);
+    } else {
+      res.status(500);
+    }
+    res.send();
+  } else {
+    res.status(404).send();
+  }
+});
