a97478c63d39238a61931381783a1bf3b73928c2
superpuper32 <pochitalin_m@mail.ru>
2018-05-17 15:45:08 +0300

    checkMillionDollarIdea middleware

--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,5 +1,11 @@
 const checkMillionDollarIdea = (req, res, next) => {
-  
+  const { numWeeks, weeklyRevenue } = req.body;
+  const totalYield = Number(numWeeks) * Number(weeklyRevenue);
+  if (totalYield < 1000000 || !numWeeks || !weeklyRevenue || isNaN(totalYield)) {
+    res.status(400).send();
+  } else {
+    next();
+  }
 };
 
 // Leave this exports assignment so that the function can be used elsewhere
